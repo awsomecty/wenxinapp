e574ac1edd925c6c1185ee4516f30989da8282c9
cty <2693196915@qq.com>
2024-10-13 13:10:56 +0800

    更改配置错误

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"hello-6garvbawf7c92762","alias":"hello"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Tencent\微信web开发者工具\cli.bat" cloud functions deploy --e hello-6garvbawf7c92762 --n quickstartFunctions --r --project "C:\Users\26931\WeChatProjects\miniprogram-1" --report_first --report
